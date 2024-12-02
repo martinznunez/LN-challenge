e45130bc5f934543ec99b1bfff7b971fc100de06
martin <nunez.86martin@gmail.com>
2024-12-02 09:15:32 -0300

    add promise to params

--- src/app/tema/[slug]/page.tsx
+++ src/app/tema/[slug]/page.tsx
@@ -1,27 +1,26 @@
-
 import { Suspense } from "react";
-
 import { Loading } from "@/components";
 import { SlugGrid } from "@/modules/slugGrid";
 
-
 interface SlugPageProps {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 
 export default async function SlugPage({ params }: SlugPageProps) {
+ 
+  const resolvedParams = await params;
+  const { slug } = resolvedParams;
+ 
 
-  
   return (
     <div className="lay-sidebar">
       <section className="sidebar__main">
         <Suspense fallback={<Loading />}>
-          <SlugGrid slug={params.slug} />
+          <SlugGrid slug={slug} />
         </Suspense>
       </section>
     </div>
   );
 }
-
